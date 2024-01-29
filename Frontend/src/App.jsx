21b3ab6eb0d9b88650f526dc226a285dcfa3494a
Sakar Vaidya <sakarvaidya72527@gmail.om>
2024-01-29 15:23:05 +0530

    frontend routes and nav

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import Layout from './components/Layout.jsx'
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import { Outlet } from 'react-router-dom';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-
-      <h1>Vite + React</h1>
-
+     <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <Layout />
+      <Outlet />
+      </Box>
     </>
   )
 }
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,19 +3,33 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider, createTheme } from "@mui/material";
-import { BrowserRouter } from "react-router-dom";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+
+import HomeScreen from './pages/HomeScreen.jsx'
 const theme = createTheme({
   typography: {
     fontFamily: "Poppins, sans-serif",
   },
 });
 
+const router =createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={ <App/>}>
+    <Route index={true} path="/" element={ <HomeScreen/>}/>
+    </Route>
+  )
+)
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
       <ThemeProvider theme={theme}>
-        <App />
+        <RouterProvider router={router}/>
       </ThemeProvider>
-    </BrowserRouter>
   </React.StrictMode>
 );
